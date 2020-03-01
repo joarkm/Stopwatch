8371befb94224d8060190bc95f419207186c7788
Joar K. Mauseth <jkm99412@gmail.com>
2020-03-01 20:58:59 +0100

    Added @angular/material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StopwatchModule } from './stopwatch/stopwatch.module';
 import { TimerModule } from './timer/timer.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { TimerModule } from './timer/timer.module';
     BrowserModule,
     AppRoutingModule,
     StopwatchModule,
-    TimerModule
+    TimerModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
